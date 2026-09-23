464733625fa31dec16de86b916e437940c628b2d
Jeremy <jeremy.frank@students.snow.edu>
2026-09-23 10:20:03 -0600

    Complete employee inheritance lab

---
+++ Employee.cs
@@ -0,0 +1,47 @@
+﻿public class Employee
+{
+
+    public string Name { get; set; }
+    public decimal BaseSalary { get; set; }
+
+
+    public Employee(string name, decimal baseSalary)
+    {
+        Name = name;
+        BaseSalary = baseSalary;
+    }
+
+    public virtual decimal CalculateSalary()
+    {
+        return BaseSalary;
+    }
+}
+
+public class FullTimeEmployee : Employee
+{
+    public decimal Bonus { get; set; }
+    public FullTimeEmployee(string name, decimal baseSalary, decimal bonus)
+        : base(name, baseSalary)
+    {
+        Bonus = bonus;
+    }
+    public override decimal CalculateSalary()
+    {
+        return BaseSalary + Bonus;
+    }
+}
+public class PartTimeEmployee : Employee
+{
+    public int HoursWorked { get; set; }
+    public decimal HourlyRate { get; set; }
+    public PartTimeEmployee(string name, int hoursWorked, decimal hourlyRate)
+        : base(name, 0)
+    {
+        HoursWorked = hoursWorked;
+        HourlyRate = hourlyRate;
+    }
+    public override decimal CalculateSalary()
+    {
+        return HoursWorked * HourlyRate;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("Bob", 50000, 5000);
+PartTimeEmployee partTimeEmployee = new PartTimeEmployee("Sue", 100, 20);
+
+
+Console.WriteLine($"Full-time employee {fullTimeEmployee.Name}\n Salary: ${fullTimeEmployee.CalculateSalary():N2}\n");
+Console.WriteLine($"Part-time employee {partTimeEmployee.Name}\n Salary: ${partTimeEmployee.CalculateSalary():N2}");
